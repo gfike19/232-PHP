b8c3059f37d5a2d18074650aab948f7bbc94faa1
gfike19@gmail.com <Vaughn*573>
2019-08-28 15:56:07 -0500

    did some more work

--- Unit5/CP.php
+++ Unit5/CP.php
@@ -13,5 +13,28 @@ foreach ($produceList as $value) {
 fclose($f);
 
 function FileLineCount($fileName) {
+    $f = fopen($fileName, "r");
+    $lines = fread($f, filesize($fileName));
+    fclose($f);
+    $arr = explode("\n", $lines);
+    $count = 0;
+    // offset is needed as last line is empty
+    // real world would check for this with conditional
+    while($count < sizeof($arr) - 1){
+        $count++;
+    }
+    return $count;
 }
+ echo "<b>Task 1</b><br>There are ".FileLineCount("ProducePrice.txt")." products in the file.";
+
+$more = array("peppers 0.99","celery 1.29","cabbage 0.79","tomatoes 1.19");
+$newfile = fopen("ProducePrice.txt", "a+");
+foreach ($more as $value) {
+    fwrite($newfile, $value);
+    fwrite($newfile, "\n");
+}
+fclose($newfile);
+
+echo "<br><b>Task 2</b><br>There are ".
+FileLineCount("ProducePrice.txt")." products in the file.";
 ?>

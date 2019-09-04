739c3659e7e62cddcde0ec19332b31d9e9a20f90
gfike19@gmail.com <Vaughn*573>
2019-09-04 14:02:26 -0500

    did most of the exercises

--- unit6/Unit6_Exer.php
+++ unit6/Unit6_Exer.php
@@ -27,6 +27,7 @@ assert_options(ASSERT_WARNING, 0);
 assert_options(ASSERT_QUIET_EVAL, 1);
 
 // Create a handler function
+// line below is needed to take in info from assert
 function my_assert_handler($file, $line, $code, $desc = null)
 {
     // echo "Assertion failed at $file:$line: $code";
@@ -51,9 +52,26 @@ echo "<br><b>Exercise 2</b><br>";
 $num1 = $_POST["num1"];
 assert($num1 > 0, "Number must be greater than zero");
 echo "<br><b>Exercise 3</b><br>";
-$num2 = $_POST["num1"];
+$num2 = $_POST["num2"];
 assert($num2 > 25, "Number must be greater than twenty five");
 echo "<br><b>Exercise 4</b><br>";
-$num3 = $_POST["num1"];
+$num3 = $_POST["num3"];
 assert($num3 < 5, "Number must be less than five");
+echo "<br><b>Exercise 5</b><br>";
+$num4 = $_POST["num4"];
+assert($num4 <= 10, "Number must be less than or equal to 10");
+echo "<br><b>Exercise 6</b><br>";
+$num5 = $_POST["num5"];
+assert($num5 == 10, "Number must be equal to seven");
+echo "<br><b>Exercise 7</b><br>";
+$num6 = $_POST["num6"];
+assert($num6 != 3, "Number must not be equal to three");
+
+function divByZero($num) {
+    return $num/0;
+}
+
+function arrOutbound($arr) {
+    return $arr[-1];
+}
 ?>

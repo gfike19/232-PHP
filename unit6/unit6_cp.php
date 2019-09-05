a50d43fc34158a1ee089e386b0dcdbd31bd43ef0
gfike19@gmail.com <Vaughn*573>
2019-09-05 12:14:33 -0500

    did some more work on the second half of the assignment

--- unit6/unit6_cp.php
+++ unit6/unit6_cp.php
@@ -11,13 +11,36 @@ function my_assert_handler($file, $line, $code, $desc = null)
     }
 }
 
-// Set up the callback
 assert_options(ASSERT_CALLBACK, 'my_assert_handler');
-echo "<b>Task 1</b><br>";
+
 function RangeCheck($num1) {
     assert($num1 < 1 && $num1 > 25, "RangeCheck passed");
 }
 
+function CheckForNull($value){
+    assert($value == "" || $value == " ", "CheckForNull passed" );
+}
+
+//TODO tasks 3 and 4
+
+function DivideByZero($num1, $num2){
+    if($num2 == 0) {
+        throw new Exception('Division by zero.'); 
+    }
+}
+
+echo "<b>Task 1</b><br>";
 $num1 = $_POST["num1"];
 RangeCheck($num1);
+
+echo "<br><b>Task 2</b><br>";
+$value = $_POST["val"];
+CheckForNull($value);
+
+echo "<br><b>Task 5</b><br>";
+try{
+    DivideByZero(42, 0);
+}catch (Exception $e){
+    echo $e.getMessage();
+}
 ?>

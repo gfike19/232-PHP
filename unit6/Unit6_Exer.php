049237148b1f5695ca78264562b1e9f99da478a9
gfike19@gmail.com <Vaughn*573>
2019-09-05 11:25:36 -0500

    finished exercises

--- unit6/Unit6_Exer.php
+++ unit6/Unit6_Exer.php
@@ -22,47 +22,50 @@ function my_assert_handler($file, $line, $code, $desc = null)
 // Set up the callback
 assert_options(ASSERT_CALLBACK, 'my_assert_handler');
 
-// $uInput = $_POST["uInput"];
-// echo "<b>Exercise 1</b><br>";
-// // Make an assertion that should fail
-// assert($uInput != null, "Parameter must not be null");
-// echo "<br><b>Exercise 2</b><br>";
-// $num1 = $_POST["num1"];
-// assert($num1 > 0, "Number must be greater than zero");
-// echo "<br><b>Exercise 3</b><br>";
-// $num2 = $_POST["num2"];
-// assert($num2 > 25, "Number must be greater than twenty five");
-// echo "<br><b>Exercise 4</b><br>";
-// $num3 = $_POST["num3"];
-// assert($num3 < 5, "Number must be less than five");
-// echo "<br><b>Exercise 5</b><br>";
-// $num4 = $_POST["num4"];
-// assert($num4 <= 10, "Number must be less than or equal to 10");
-// echo "<br><b>Exercise 6</b><br>";
-// $num5 = $_POST["num5"];
-// assert($num5 == 10, "Number must be equal to seven");
-// echo "<br><b>Exercise 7</b><br>";
-// $num6 = $_POST["num6"];
-// assert($num6 != 3, "Number must not be equal to three");
+$uInput = $_POST["uInput"];
+echo "<b>Exercise 1</b><br>";
+// Make an assertion that should fail
+assert($uInput != null, "Parameter must not be null");
+echo "<br><b>Exercise 2</b><br>";
+$num1 = $_POST["num1"];
+assert($num1 > 0, "Number must be greater than zero");
+echo "<br><b>Exercise 3</b><br>";
+$num2 = $_POST["num2"];
+assert($num2 > 25, "Number must be greater than twenty five");
+echo "<br><b>Exercise 4</b><br>";
+$num3 = $_POST["num3"];
+assert($num3 < 5, "Number must be less than five");
+echo "<br><b>Exercise 5</b><br>";
+$num4 = $_POST["num4"];
+assert($num4 <= 10, "Number must be less than or equal to 10");
+echo "<br><b>Exercise 6</b><br>";
+$num5 = $_POST["num5"];
+assert($num5 == 10, "Number must be equal to seven");
+echo "<br><b>Exercise 7</b><br>";
+$num6 = $_POST["num6"];
+assert($num6 != 3, "Number must not be equal to three");
 
 function divByZero($num) {
     return $num/0;
 }
-$arr = array(19, 23, 42, 16);
+$arr = array();
+$arr[0] = 19;
+$arr[1] = 23;
+$arr[2] = 42;
 function arrOutbound($arr, $num) {
     return  $arr[$num];
 }
 
-function intOverflow($num){
-    return chr($num);
-}
+// function runtime($num){
+//     $arr[4] = $num;
+// }
 
 $num7 = $_POST["num7"];
 
 try{
-    // divByZero($num7);
-    // arrOutbound($arr, $num7);
-    intOverflow($num7);
+    divByZero($num7);
+    arrOutbound($arr, $num7);
+    funcCall();
 }
 catch(Exception $e) {
     echo $e.getMessage();

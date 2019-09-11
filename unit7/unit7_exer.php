9dabecf93ca53909ec173ec4ea2b197f54bd0522
gfike19@gmail.com <Vaughn*573>
2019-09-11 13:50:47 -0500

    did some more work on unit7

--- unit7/unit7_exer.php
+++ unit7/unit7_exer.php
@@ -1,7 +1,7 @@
 <?php
 
 $errorLog = fopen("errorLog.txt", "w");
-$errorLog.close();
+fclose($errorLog);
 
 function DisplayLogfile() {
     $errorFile = fopen("errorLog.txt", "r");
@@ -13,20 +13,22 @@ class Errors{
         error_log(2/0, 3, "errorLog.txt");
         addNewLine();
     }
-
-    function FileDoesNotExist() {
-        error_log($f = fopen("test.txt", "w"), 3, "errorLog.txt");
-        addNewLine();
-    }
-
     function arrayOutOfBounds() {
-        $arr = array(57, 39, 61);
-        error_log($arr[4], 3, "errorLog.txt");
-        addNewLine();
-    }
+            $arr = array(57, 39, 61);
+            error_log($arr[4], 3, "errorLog.txt");
+            addNewLine();
+        }
+    // function FileDoesNotExist() {
+    //     error_log(fopen("test.txt", "w"), 3, "errorLog.txt");
+    //     addNewLine();
+    // }
+
+    
 
     function arrayIsNull(){
-        
+        $arr = array();
+        error_log(sizeof($arr), 3, "errorLog.txt");
+        addNewLine();
     }
 }
 
@@ -35,4 +37,32 @@ function addNewLine (){
     fwrite($errorLog, "\n");
     fclose($errorLog);
 }
+
+$e = new Errors();
+
+try {
+    $e ->arrayIsNull();
+} catch(Exception $e){
+
+}
+
+try{
+    $e ->arrayOutOfBounds();
+} catch(Exception $e){
+
+}
+
+try{
+    $e ->divideByZero();
+}catch(Exception $e) {
+
+}
+
+// try{
+//     $e ->FileDoesNotExist();
+// } catch(Exception $e) {
+    
+// }
+
+DisplayLogfile();
 ?>
